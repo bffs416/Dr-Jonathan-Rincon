fbddbbc00d791849f11342a03abc21ddef4071db
Pipe <bffs.16.04.95@gmail.com>
2025-09-25 22:38:15 +0000

    esto sirve de algo ?@import url('https://fonts.googleapis.com/css2?famil

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['PT Sans', 'sans-serif'],
-        headline: ['Playfair Display', 'serif'],
+        body: ['Outfit', 'sans-serif'],
+        headline: ['Outfit', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
