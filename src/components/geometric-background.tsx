8e64bb6a5dad4ec6a2f6755e370762b800b91d23
Pipe <bffs.16.04.95@gmail.com>
2025-09-25 23:04:15 +0000

    el poligono que gira, puedes hacer muchos mas ?

--- src/components/geometric-background.tsx
+++ src/components/geometric-background.tsx
@@ -29,8 +29,20 @@ export function GeometricBackground() {
       <div className="absolute bottom-[-25%] right-[-15%] w-[40rem] h-[40rem] animate-float animation-delay-3000 opacity-30">
         <Cube className="animate-rotate" />
       </div>
-       <div className="absolute top-[10%] right-[5%] w-48 h-48 animate-float animation-delay-6000 opacity-40">
+      <div className="absolute top-[10%] right-[5%] w-48 h-48 animate-float animation-delay-6000 opacity-40">
         <Cube className="animate-rotate [animation-duration:30s]" />
+      </div>
+      <div className="absolute bottom-[5%] left-[10%] w-32 h-32 animate-float animation-delay-1000 opacity-20">
+        <Cube className="animate-rotate [animation-duration:40s]" />
+      </div>
+      <div className="absolute top-[40%] left-[30%] w-64 h-64 animate-float animation-delay-4000 opacity-10">
+        <Cube className="animate-rotate [animation-duration:25s]" />
+      </div>
+       <div className="absolute bottom-[15%] right-[35%] w-24 h-24 animate-float animation-delay-8000 opacity-25">
+        <Cube className="animate-rotate [animation-duration:35s]" />
+      </div>
+       <div className="absolute top-[5%] right-[40%] w-40 h-40 animate-float animation-delay-2000 opacity-15">
+        <Cube className="animate-rotate [animation-duration:45s]" />
       </div>
     </div>
   );
