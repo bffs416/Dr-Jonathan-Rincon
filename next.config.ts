da2fdec0cdda8cd7a5b1d7e34cec3a34282ba47a
Pipe <bffs.16.04.95@gmail.com>
2025-09-26 03:17:02 +0000

    si, se encuentra en la carpeta public

--- next.config.ts
+++ next.config.ts
@@ -23,27 +23,12 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'https,
         hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
       },
     ],
-  },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.(mp4|webm)$/,
-      use: {
-        loader: 'file-loader',
-        options: {
-          publicPath: '/_next/static/videos/',
-          outputPath: 'static/videos/',
-          name: '[name].[hash].[ext]',
-        },
-      },
-    });
-
-    return config;
   },
 };
 
